7c24b2324c1995f074b5cb3e7fb7727ce41679c2
ChandraShekar2001 <pvchandu07@gmail.com>
2024-05-01 20:23:20 +0530

    Added gig routes and controllers

--- src/server.ts
+++ src/server.ts
@@ -13,24 +13,23 @@ import { StatusCodes } from 'http-status-codes';
 import { config } from '@gateway/config';
 import { elasticSearch } from '@gateway/elasticsearch';
 import { appRoutes } from '@gateway/routes';
-// import { axiosAuthInstance } from '@gateway/services/api/auth.service';
-// import { axiosBuyerInstance } from '@gateway/services/api/buyer.service';
-// import { axiosSellerInstance } from '@gateway/services/api/seller.service';
-// import { axiosGigInstance } from '@gateway/services/api/gig.service';
-// import { Server } from 'socket.io';
-// import { createClient } from 'redis';
-// import { createAdapter } from '@socket.io/redis-adapter';
-// import { SocketIOAppHandler } from '@gateway/sockets/socket';
+import { axiosAuthInstance } from '@gateway/services/api/auth.service';
+import { axiosBuyerInstance } from '@gateway/services/api/buyer.service';
+import { axiosSellerInstance } from '@gateway/services/api/seller.service';
+import { axiosGigInstance } from '@gateway/services/api/gig.service';
+import { Server } from 'socket.io';
+import { createClient } from 'redis';
+import { createAdapter } from '@socket.io/redis-adapter';
+import { SocketIOAppHandler } from '@gateway/sockets/socket';
 // import { axiosMessageInstance } from '@gateway/services/api/message.service';
 // import { axiosOrderInstance } from '@gateway/services/api/order.service';
 // import { axiosReviewInstance } from '@gateway/services/api/review.service';
 import { isAxiosError } from 'axios';
-import { axiosAuthInstance } from '@gateway/services/api/auth.service';
 
 const SERVER_PORT = 4000;
 const DEFAULT_ERROR_CODE = 500;
 const log: Logger = winstonLogger(`${config.ELASTIC_SEARCH_URL}`, 'apiGatewayServer', 'debug');
-// export let socketIO: Server;
+export let socketIO: Server;
 
 export class GatewayServer {
   private app: Application;
@@ -69,9 +68,9 @@ export class GatewayServer {
     app.use((req: Request, _res: Response, next: NextFunction) => {
       if (req.session?.jwt) {
         axiosAuthInstance.defaults.headers['Authorization'] = `Bearer ${req.session?.jwt}`;
-        // axiosBuyerInstance.defaults.headers['Authorization'] = `Bearer ${req.session?.jwt}`;
-        // axiosSellerInstance.defaults.headers['Authorization'] = `Bearer ${req.session?.jwt}`;
-        // axiosGigInstance.defaults.headers['Authorization'] = `Bearer ${req.session?.jwt}`;
+        axiosBuyerInstance.defaults.headers['Authorization'] = `Bearer ${req.session?.jwt}`;
+        axiosSellerInstance.defaults.headers['Authorization'] = `Bearer ${req.session?.jwt}`;
+        axiosGigInstance.defaults.headers['Authorization'] = `Bearer ${req.session?.jwt}`;
         // axiosMessageInstance.defaults.headers['Authorization'] = `Bearer ${req.session?.jwt}`;
         // axiosOrderInstance.defaults.headers['Authorization'] = `Bearer ${req.session?.jwt}`;
         // axiosReviewInstance.defaults.headers['Authorization'] = `Bearer ${req.session?.jwt}`;
@@ -104,13 +103,12 @@ export class GatewayServer {
 
     app.use((error: IErrorResponse, _req: Request, res: Response, next: NextFunction) => {
       if (error instanceof CustomError) {
-        // log.log('error', `GatewayService ${error.comingFrom}:`, error);
+        log.log('error', `GatewayService :`, error);
         res.status(error.statusCode).json(error.serializeErrors());
       }
 
       if (isAxiosError(error)) {
-        // console.log(error);
-        // log.log('error', `GatewayService Axios Error - ${error?.response?.data?.comingFrom}:`, error);
+        log.log('error', `GatewayService Axios Error - ${error?.response?.data?.comingFrom}:`, error);
         res.status(error?.response?.data?.statusCode ?? DEFAULT_ERROR_CODE).json({ message: error?.response?.data?.message ?? 'Error occurred.' });
       }
 
@@ -121,28 +119,28 @@ export class GatewayServer {
   private async startServer(app: Application): Promise<void> {
     try {
       const httpServer: http.Server = new http.Server(app);
-    //   const socketIO: Server = await this.createSocketIO(httpServer);
+      const socketIO: Server = await this.createSocketIO(httpServer);
       this.startHttpServer(httpServer);
-    //   this.socketIOConnections(socketIO);
+      this.socketIOConnections(socketIO);
     } catch (error) {
       log.log('error', 'GatewayService startServer() error method:', error);
     }
   }
 
-  // private async createSocketIO(httpServer: http.Server): Promise<Server> {
-  //   // const io: Server = new Server(httpServer, {
-  //   //   cors: {
-  //   //     origin: `${config.CLIENT_URL}`,
-  //   //     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS']
-  //   //   }
-  //   // });
-  //   const pubClient = createClient({ url: config.REDIS_HOST });
-  //   const subClient = pubClient.duplicate();
-  //   await Promise.all([pubClient.connect(), subClient.connect()]);
-  //   // io.adapter(createAdapter(pubClient, subClient));
-  //   // socketIO = io;
-  //   // return io;
-  // }
+  private async createSocketIO(httpServer: http.Server): Promise<Server> {
+    const io: Server = new Server(httpServer, {
+      cors: {
+        origin: `${config.CLIENT_URL}`,
+        methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS']
+      }
+    });
+    const pubClient = createClient({ url: config.REDIS_HOST });
+    const subClient = pubClient.duplicate();
+    await Promise.all([pubClient.connect(), subClient.connect()]);
+    io.adapter(createAdapter(pubClient, subClient));
+    socketIO = io;
+    return io;
+  }
 
   private async startHttpServer(httpServer: http.Server): Promise<void> {
     try {
@@ -155,8 +153,8 @@ export class GatewayServer {
     }
   }
 
-//   private socketIOConnections(io: Server): void {
-//     const socketIoApp = new SocketIOAppHandler(io);
-//     socketIoApp.listen();
-//   }
+  private socketIOConnections(io: Server): void {
+    const socketIoApp = new SocketIOAppHandler(io);
+    socketIoApp.listen();
+  }
 }
